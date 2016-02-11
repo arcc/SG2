06c8d27861800e257434c9a7b04666ed8249eb1b
luojing1211 <jingluo@Jings-MacBook-Air-4.local>
2016-02-10 23:47:07 -0600

    get information from webpage and translate to data

--- getIMG/sg2_img.py
+++ getIMG/sg2_img.py
@@ -3,6 +3,9 @@
 # Author : Jing, Aldo, and sg2 members
 import urllib
 import os
+import re
+import time
+from datetime import datetime
 
 urlbase = 'http://eol.jsc.nasa.gov/DatabaseImages/ESC'
 infopage = 'http://eol.jsc.nasa.gov/SearchPhotos/photo.pl?'
@@ -17,9 +20,29 @@ class ASTRO_IMG(object):
             self.mission = mission
         self.image_url = self.get_url()
         self.page_url = self.get_page_url()
+        self.image_info_pattern = {'ISS nadir point':
+            re.compile(r'ISS nadir point:</b></em> \d+.+\d+&deg;+ [A-Z]+'),
+                                   'Photo center point':
+            re.compile(r'Photo center point:</b></em> \d+.+\d+&deg;+ [A-Z]+'),
+                                   'Spacecraft Altitude':
+            re.compile(r'\d+ +nautical miles'),
+                           'Focal Length':
+            re.compile(r'\d+mm'),
+                           'Date taken':
+            re.compile(r'\d+.+\d+.+\d'),
+                           'Time taken':
+            re.compile(r'\d+:+\d+:+\d+ +GMT')}
+        self.image_info = {'ISS nadir point': '',
+                           'Photo center point': '',
+                           'Spacecraft Altitude': '',
+                           'Focal Length': '',
+                           'Date taken': '',
+                           'Time taken': ''}
+
         self.img_category = None
-
-
+        self.get_image_info()
+        self.info_list = ['nadir', 'photo_center', 'focal_length',
+                          'time_taken', 'img_category', 'spacecraft_alt']
 
     def get_mission(self, image_id):
         """Get astronaut photo taken mission for image id
@@ -53,7 +76,77 @@ class ASTRO_IMG(object):
         id_part = 'roll=E&frame=' + id_num
         page_url = infopage + mission_part + '&' + id_part
         return page_url
-        
+
+    def get_image_info(self):
+        """Get the image information from the webpage"""
+        page = urllib.urlopen(self.page_url)
+        htmlstr = page.read()
+        if 'removed' in htmlstr:
+            raise ValueError('The Page URL for image ' + self.image_id +
+                             ' is invalid.')
+        for keywords in self.image_info.keys():
+            startIndex =  htmlstr.find(keywords)
+            if startIndex == -1:
+                raise RuntimeError('Can not find string ' + keywords + ' in '
+                                    'the webpage.')
+            search_length = 100
+            pattern = self.image_info_pattern[keywords]
+            search_str = htmlstr[startIndex - search_length :startIndex + search_length]
+            match = re.findall(pattern, search_str)
+            if match != []:
+                if '</b></em>' in match[0]:
+                    result = match[0].split('</em>')[1]
+                else:
+                    result = match[0]
+                self.image_info[keywords] = result
+
+    def translate_info_as_data(self):
+        """This function takes image_info attrbution and set attrbutions to the
+        right format
+        """
+        # get focus lenght in unit of mm
+        setattr(self,'focal_length', float(self.image_info['Focal Length'][:-2]))
+        # get Altitude in unit of km
+        alt_str = self.image_info['Spacecraft Altitude']
+        alt_str = alt_str.split()
+        alt_nautical_mile = float(alt_str[0])
+        alt_km = alt_nautical_mile*1.852
+        setattr(self, 'spacecraft_alt', alt_km)
+        # get data and time
+        date_str = self.image_info['Date taken']
+        time_str = self.image_info['Time taken']
+        date_str = date_str.split('.')
+        time_str = time_str.split(':')
+        date = [int(x) for x in date_str]
+        time = [int(x) for x in time_str[0:2]]
+        time.append(int(time_str[2][0:2]))
+        setattr(self, 'time_taken', datetime(date[0], date[1], date[2],
+                                             time[0], time[1], time[2]))
+        # Process nadir and center point
+        ndstr = self.image_info['ISS nadir point']
+        ctstr = self.image_info['Photo center point']
+        if ndstr != '':
+            ndstr = ndstr.split(',')
+            latstr = self.process_latlon_str(ndstr[0])
+            lonstr = self.process_latlon_str(ndstr[1])
+            setattr(self, 'nadir', [latstr,lonstr])
+        else:
+            setattr(self, 'nadir', [])
+
+        if ctstr != '':
+            ctstr = ctstr.split(',')
+            latstr = self.process_latlon_str(ctstr[0])
+            lonstr = self.process_latlon_str(ctstr[1])
+            setattr(self, 'photo_center', [latstr,lonstr])
+        else:
+            setattr(self, 'photo_center', [])
+
+    def process_latlon_str(self,lstr):
+        """get latitude and longitude from image string
+        """
+        ldata = lstr.replace('&deg;', '')
+        return ldata.lstrip()
+
     # This secret discovered by Aldo
     def download_image(self, download_path=None):
         """Save the image from url.
