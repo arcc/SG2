be833f161be43cb0d7a47a0345e9e638e0332898
luojing1211 <jingluo@Jings-MacBook-Air-4.local>
2016-02-15 09:37:49 -0600

    Fix bug, add test

--- sg2_users/user.py
+++ sg2_users/user.py
@@ -2,9 +2,9 @@
 class USER(object):
     """This is a class for sg2 user
     """
-    def __init__(self, username, email):
+    def __init__(self, username, **kwargs):
         self.name = username
-        self.email = email
+        self.email = None
         self.privilege_level = None
         self.num_images_processed = 0
         self.accurate_rate = 0.0
