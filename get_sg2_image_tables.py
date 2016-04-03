47680bbf6d8c38f4f044d97eac655f7a3b7325ed
luojing1211 <jingluo@Jings-MacBook-Air-4.local>
2016-04-03 11:06:59 -0500

    Fix bug and update

--- get_sg2_image_tables.py
+++ get_sg2_image_tables.py
@@ -2,13 +2,7 @@
 """
 This is a python script for getting database project tables.
 useage:
-    $python get_next_image_url.py username project_name index_in_db
-    or
-    $python get_next_image_url.py username project_name index_in_db max_rate
-Return:
-    print [small image url, large image url, index]
-    If the index execcds from the database
-    print ['-1', '-1', index]
+    $python get_sg2_image_table.py username project_name index_in_db
 """
 from core.sg2_category import sg2_category as sg2c
 from core.database.sg2_database_utils import image_database
@@ -27,4 +21,5 @@ def get_sg2_img_tables(table_prefix):
     return sg2_tables
 
 if __name__== "__main__":
-    print get_sg2_img_tables("sg2")
+    prefix = sys.argv[1]
+    print get_sg2_img_tables(prefix)
