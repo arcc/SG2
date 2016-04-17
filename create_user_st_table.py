fa1fbdb598001330694fa6a3eace0e3810f21dab
luojing1211 <jingluo@Jings-MacBook-Air-4.local>
2016-04-17 18:45:28 -0500

    delete unused files

--- create_user_st_table.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/python
-"""
-"""
-from core.sg2_category import sg2_category as sg2c
-from core.database.users_database_utils import users_database
-
-db = users_database(password='root')
-db.create_statistics_tables()
