752b26613ca4f1c3d0e0a185df3d1cca517814b3
luojing1211 <jingluo@Jings-MacBook-Air-4.local>
2016-04-21 15:33:08 -0500

    fix a bug

--- get_next_img_url.py
+++ get_next_img_url.py
@@ -42,14 +42,14 @@ def get_next_image_url(username, index_in_db, project_name=''):
     total_img = imc.database.get_total_num_image(img_table)
 
     if index_in_db > total_img:
-         return json.dumps(('-1', '-1', index_in_db))
+        return json.dumps(('-1', '-1', index_in_db))
     if index_in_db == 0:
-         index_in_db = 1
+        index_in_db = 1
 
     under_rate_image = imc.database.get_table_element(img_table, 'image_index, project, image_ID, '
                         'number_rated, max_rate', 'number_rated<max_rate AND image_index>=%d'%index_in_db)
     if under_rate_image ==[]:
-        return json.dumps(('-1', '-1', index_in_db))
+        return json.dumps(('-1', '-1', '-1',index_in_db))
 
     condition = ("SELECT image_index FROM %s WHERE number_rated<max_rate and"
                  " image_index>=%d")%(img_table, index_in_db)
@@ -65,7 +65,11 @@ def get_next_image_url(username, index_in_db, project_name=''):
         under_rated.add(unimg[0])
 
     usr_unrate = under_rated.symmetric_difference(rated)
-
+    print rated
+    print under_rated
+    print usr_unrate
+    if list(usr_unrate) == []:
+        return json.dumps(('-1', '-1', '-1',index_in_db))
     target_index = min(list(usr_unrate))
     #result_image_index = imc.database.get_table_element(img_table, 'image_index', "image_ID='%s'"%target_id)[0][0]
     imc.get_image_from_database(index=target_index)
